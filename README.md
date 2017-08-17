2fb02b822e5371fa59ecc1483cc41979830ce081
Unknown <osher.filter@gmail.com>
2017-08-17 16:56:48 +0300

    savepoint - scafold complete

--- README.md
+++ README.md
@@ -2,18 +2,28 @@
 
 ## Overview
 
+The server can play in dev env the part of integration partners who publish their API using open-api spec (fka swagger).
+
+The server is a CLI tool (may be installed globally) that is run with a swagger document (which may be in a number of requirable formats: JSON, JS, YAML)
+
+> Do we want to support blueprint / RAML as well?
+>      we could do it with a XXX-to-swagger adapter
+
+Once started, the server will reply to any path + verb pair (which basically correspond to operation-id) with a prepared reply based on the success response for that operation as stated in swagger of the integration partners.
+So - once started - you’re ready for integration.
+
+Next - developers can challenge their integration by using a special API to set the server on runtime to return whatever responses they need for whatever operation.
+
+Reply management API is a post to a configurable path.
 
 
-## General Info
-
- Package     | **  **
- ----------- | --------------------------
- Team        | ** TBD! **
- Branch      | ** CWT Digital **
- Team Leader | ** [Oren Bregman](https://carlsonwagonlit.jiveon.com/people/U005OXB) **
- Owner       | ** [Osher El-Netanany](https://carlsonwagonlit.jiveon.com/people/u002oxe) **
 
 
-## Docs
+## Response setting API: 
+An idiomatic operation that accepts a structure that maps paths + verbs to array of response descriptors.
 
-[npm-package: oas-mocker](https://carlsonwagonlit.jiveon.com/docs/DOC-????)
+
+
+
+
+
--- bin/cli.js
+++ bin/cli.js
@@ -1,19 +1,42 @@
-var args    = require('../lib/args')(process, console)
-  , log     = require('../lib/logger')
+const args    = require('../lib/args')(process, console.log)
+const log     = 
+  require('../lib/logger')
     .configure(
-      { appenders:
-        { stdout:  { type: "console" } }
-      , categories: {
-          default: { appenders: [ 'stdout' ], level: 'info' }
-        }
+      { appenders:  { stdout:  { type: "console" } }
+      , categories: { default: { appenders: [ 'stdout' ], level: args.logLevel } }
       }
     ).of('oas:cli')
-  , figlet  = require('figlet')
-  ;
+const banners = require('../lib/cli-banners')(log)
 
-log.info('\n%s\nOpen API Server Mocker', figlet.textSync('OASM'));
+banners.hello()
 log.debug('accepted args', args);
 
-require('../lib')(args, function(err) {
-    log[ /* istanbul ignore next */ err ? "error" : "info"](err || "Ok :)")
-});
+const svr = require('../lib')({args, log})
+  .on('started', address => log.info('server started', address))
+  .on('error', banners.error)
+  .on('fatal', err => { 
+      banners.fatal(err)
+      process.exit(1)
+  })
+
+
+process.on('SIGINT', shutdown('SIGINT'))
+process.on('SIGTERM', shutdown('SIGTERM'))
+
+function shutdown(signal) {
+  return function handleSignal(err) {
+      if (process.shuttingDown) return
+      
+      banners.bye(signal)
+      svr.close((err) => {
+          log[err ? 'error' : 'info']('server shutdown ', err || 'OK')
+      })
+      setTimeout(
+        () => {
+          log.warn('server did not close timely - I had to kick it :(') 
+          process.exit(1)
+        }
+      , args.shutdownGrace
+      ).unref()
+  }
+}
